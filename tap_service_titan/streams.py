60cd476d24d49649d67dc8b5817a43a408a1ea6f
Cody J. Hanson <cody@codyjhanson.com>
2024-05-22 13:58:29 -0500

    modifiedOn -> DateTimeType

--- tap_service_titan/streams.py
+++ tap_service_titan/streams.py
@@ -476,7 +476,7 @@ class InvoicesStream(ServiceTitanStream):
 
     name = "invoices"
     primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key: str = "modifiedOn"
+    #     replication_key: str = "modifiedOn"
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
     schema = th.PropertiesList(
@@ -558,7 +558,7 @@ class InvoicesStream(ServiceTitanStream):
         ),
         th.Property("depositedOn", th.StringType),
         th.Property("createdOn", th.StringType),
-        th.Property("modifiedOn", th.StringType),
+        th.Property("modifiedOn", th.DateTimeType),
         th.Property("adjustmentToId", th.IntegerType),
         th.Property(
             "job",
@@ -583,7 +583,7 @@ class InvoicesStream(ServiceTitanStream):
             th.ObjectType(
                 th.Property("id", th.IntegerType),
                 th.Property("name", th.StringType),
-                th.Property("modifiedOn", th.StringType),
+                th.Property("modifiedOn", th.DateTimeType),
             ),
         ),
         th.Property("commissionEligibilityDate", th.StringType),
