f6fb1248d759394b07a87a4688daddc6518f711a
Cody J. Hanson <cody@codyjhanson.com>
2024-08-19 20:53:29 -0500

    Add purchase streams

--- tap_service_titan/streams.py
+++ tap_service_titan/streams.py
@@ -1401,3 +1401,239 @@ class CampaignsStream(ServiceTitanStream):
     def path(self) -> str:
         """Return the API path for the stream."""
         return f"/marketing/v2/tenant/{self._tap.config['tenant_id']}/campaigns"
+
+
+class PurchaseOrdersStream(ServiceTitanExportStream):
+    """Define custom stream."""
+
+    name = "purchase_orders"
+    primary_keys: t.ClassVar[list[str]] = ["id"]
+    replication_key: str = "modifiedOn"
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("number", th.StringType),
+        th.Property("invoiceId", th.IntegerType),
+        th.Property("jobId", th.IntegerType),
+        th.Property("projectId", th.IntegerType),
+        th.Property("status", th.StringType),
+        th.Property("typeId", th.IntegerType),
+        th.Property("vendorId", th.IntegerType),
+        th.Property("technicianId", th.IntegerType),
+        th.Property(
+            "shipTo",
+            th.ObjectType(
+                th.Property("street", th.StringType),
+                th.Property("unit", th.StringType),
+                th.Property("city", th.StringType),
+                th.Property("state", th.StringType),
+                th.Property("zip", th.StringType),
+                th.Property("country", th.StringType),
+            ),
+        ),
+        th.Property("businessUnitId", th.IntegerType),
+        th.Property("inventoryLocationId", th.IntegerType),
+        th.Property("batchId", th.IntegerType),
+        th.Property("vendorDocumentNumber", th.StringType),
+        th.Property("date", th.StringType),
+        th.Property("requiredOn", th.StringType),
+        th.Property("sentOn", th.StringType),
+        th.Property("receivedOn", th.StringType),
+        th.Property("createdOn", th.StringType),
+        th.Property("modifiedOn", th.StringType),
+        th.Property("total", th.NumberType),
+        th.Property("tax", th.NumberType),
+        th.Property("shipping", th.NumberType),
+        th.Property("summary", th.StringType),
+        th.Property(
+            "items",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.IntegerType),
+                    th.Property("skuId", th.IntegerType),
+                    th.Property("skuName", th.StringType),
+                    th.Property("skuCode", th.StringType),
+                    th.Property("skuType", th.StringType),
+                    th.Property("description", th.StringType),
+                    th.Property("vendorPartNumber", th.StringType),
+                    th.Property("quantity", th.NumberType),
+                    th.Property("quantityReceived", th.NumberType),
+                    th.Property("cost", th.NumberType),
+                    th.Property("total", th.NumberType),
+                    th.Property(
+                        "serialNumbers",
+                        th.ArrayType(
+                            th.ObjectType(
+                                th.Property("id", th.IntegerType),
+                                th.Property("number", th.StringType),
+                            )
+                        ),
+                    ),
+                    th.Property("status", th.StringType),
+                    th.Property("chargeable", th.BooleanType),
+                )
+            ),
+        ),
+        th.Property(
+            "customFields",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("typeId", th.IntegerType),
+                    th.Property("name", th.StringType),
+                    th.Property("value", th.StringType),
+                )
+            ),
+        ),
+    ).to_dict()
+
+    @cached_property
+    def path(self) -> str:
+        """Return the API path for the stream."""
+        return f"/inventory/v2/tenant/{self._tap.config['tenant_id']}/export/purchase-orders"
+
+
+class PurchaseOrderMarkupsStream(ServiceTitanStream):
+    """Define custom stream."""
+
+    name = "purchase_order_markups"
+    primary_keys: t.ClassVar[list[str]] = ["id"]
+
+    schema = th.PropertiesList(
+        th.Property("from", th.NumberType),
+        th.Property("to", th.NumberType),
+        th.Property("percent", th.NumberType),
+        th.Property("id", th.IntegerType),
+    ).to_dict()
+
+    @cached_property
+    def path(self) -> str:
+        """Return the API path for the stream."""
+        return f"/inventory/v2/tenant/{self._tap.config['tenant_id']}/purchase-order-markups"
+
+class PurchaseOrderTypesStream(ServiceTitanStream):
+    """Define custom stream."""
+
+    name = "purchase_order_types"
+    primary_keys: t.ClassVar[list[str]] = ["id"]
+    replication_key: str = "modifiedOn"
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("name", th.StringType),
+        th.Property("active", th.BooleanType),
+        th.Property("automaticallyReceive", th.BooleanType),
+        th.Property("displayToTechnician", th.BooleanType),
+        th.Property("impactToTechnicianPayroll", th.BooleanType),
+        th.Property("allowTechniciansToSendPo", th.BooleanType),
+        th.Property("defaultRequiredDateDaysOffset", th.IntegerType),
+        th.Property("skipWeekends", th.BooleanType),
+        th.Property("excludeTaxFromJobCosting", th.BooleanType),
+        th.Property("createdOn", th.StringType),
+        th.Property("modifiedOn", th.StringType),
+    ).to_dict()
+
+
+    @cached_property
+    def path(self) -> str:
+        """Return the API path for the stream."""
+        return f"/inventory/v2/tenant/{self._tap.config['tenant_id']}/purchase-order-types"
+
+class ReceiptsStream(ServiceTitanStream):
+    """Define custom stream."""
+
+    name = "receipts"
+    primary_keys: t.ClassVar[list[str]] = ["id"]
+    replication_key: str = "modifiedOn"
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("active", th.BooleanType),
+        th.Property("number", th.StringType),
+        th.Property("vendorInvoiceNumber", th.StringType),
+        th.Property("createdById", th.IntegerType),
+        th.Property("memo", th.StringType),
+        th.Property("purchaseOrderId", th.IntegerType),
+        th.Property("billId", th.IntegerType),
+        th.Property("jobId", th.IntegerType),
+        th.Property("businessUnitId", th.IntegerType),
+        th.Property("vendorId", th.IntegerType),
+        th.Property("technicianId", th.IntegerType),
+        th.Property("inventoryLocationId", th.IntegerType),
+        th.Property(
+            "shipTo",
+            th.ObjectType(
+                th.Property("street", th.StringType),
+                th.Property("unit", th.StringType),
+                th.Property("city", th.StringType),
+                th.Property("state", th.StringType),
+                th.Property("zip", th.StringType),
+                th.Property("country", th.StringType),
+            ),
+        ),
+        th.Property("shipToDescription", th.StringType),
+        th.Property("receiptAmount", th.NumberType),
+        th.Property("taxAmount", th.NumberType),
+        th.Property("shippingAmount", th.NumberType),
+        th.Property("receivedOn", th.DateTimeType),
+        th.Property("createdOn", th.DateTimeType),
+        th.Property("modifiedOn", th.DateTimeType),
+        th.Property("batchId", th.IntegerType),
+        th.Property("syncStatus", th.StringType),
+        th.Property(
+            "items",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.IntegerType),
+                    th.Property("skuId", th.IntegerType),
+                    th.Property("name", th.StringType),
+                    th.Property("code", th.StringType),
+                    th.Property("description", th.StringType),
+                    th.Property("quantity", th.NumberType),
+                    th.Property("cost", th.NumberType),
+                    th.Property(
+                        "generalLedgerAccount",
+                        th.ObjectType(
+                            th.Property("name", th.StringType),
+                            th.Property("number", th.StringType),
+                            th.Property("type", th.StringType),
+                            th.Property("detailType", th.StringType),
+                        ),
+                    ),
+                    th.Property(
+                        "costOfSaleAccount",
+                        th.ObjectType(
+                            th.Property("name", th.StringType),
+                            th.Property("number", th.StringType),
+                            th.Property("type", th.StringType),
+                            th.Property("detailType", th.StringType),
+                        ),
+                    ),
+                    th.Property(
+                        "assetAccount",
+                        th.ObjectType(
+                            th.Property("name", th.StringType),
+                            th.Property("number", th.StringType),
+                            th.Property("type", th.StringType),
+                            th.Property("detailType", th.StringType),
+                        ),
+                    ),
+                )
+            ),
+        ),
+        th.Property(
+            "customFields",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("typeId", th.IntegerType),
+                    th.Property("name", th.StringType),
+                    th.Property("value", th.StringType),
+                )
+            ),
+        ),
+    ).to_dict()
+
+
+    @cached_property
+    def path(self) -> str:
+        """Return the API path for the stream."""
+        return f"/inventory/v2/tenant/{self._tap.config['tenant_id']}/receipts"
