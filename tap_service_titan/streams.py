b6a029d1e6d5a9cac0072222357cb2d81a8c3ecf
Cody J. Hanson <cody@codyjhanson.com>
2024-10-16 17:43:35 -0500

    Set all flags in reviews requests

--- tap_service_titan/streams.py
+++ tap_service_titan/streams.py
@@ -6,7 +6,7 @@ import sys
 import typing as t
 from datetime import datetime, timedelta, timezone
 from functools import cached_property
-
+from urllib.parse import urlencode
 import requests
 from singer_sdk import typing as th  # JSON Schema typing helpers
 from singer_sdk.helpers import types
@@ -1780,6 +1780,7 @@ class ReviewsStream(ServiceTitanStream):
     """Define reviews stream."""
 
     name = "reviews"
+    primary_keys: t.ClassVar[list[str]] = ["review", "platform", "address"]
     replication_key: str = "publishDate"
 
     schema = th.PropertiesList(
@@ -1812,6 +1813,32 @@ class ReviewsStream(ServiceTitanStream):
     def path(self) -> str:
         """Return the API path for the stream."""
         return f"/marketingreputation/v2/tenant/{self._tap.config['tenant_id']}/reviews"
+
+    def get_url_params(
+        self,
+        context: dict | None,
+        next_page_token: Any | None,  # noqa: ANN401
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params = super().get_url_params(context, next_page_token)
+        flags = [
+            "includeReviewsWithoutLocation",
+            "includeReviewsWithoutCampaign",
+            "includeReviewsWithoutTechnician",
+        ]
+        for flag in flags:
+            params[flag] = None
+        encoded = urlencode(params)
+        encoded = encoded.replace("=None", "")
+        return encoded
 
 
 # Dispatch streams
