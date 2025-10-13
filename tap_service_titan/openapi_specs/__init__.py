c03895459f0143c9ee6e50c0532c067e2f6ff203
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-10-13 12:19:14 -0600

    fix: Also make nested objects nullable

--- tap_service_titan/openapi_specs/__init__.py
+++ tap_service_titan/openapi_specs/__init__.py
@@ -52,6 +52,7 @@ def _normalize_schema(
     schema_type: str | list[str] = result.get("type", [])
 
     if "object" in schema_type:
+        result["nullable"] = len(key_properties) == 0
         for prop, prop_schema in result.get("properties", {}).items():
             prop_schema["nullable"] = prop not in key_properties
             result["properties"][prop] = _normalize_schema(prop_schema)
@@ -65,96 +66,13 @@ def _normalize_schema(
         schema_type = result.get("type", [])
 
     types = [schema_type] if isinstance(schema_type, str) else schema_type
-    if result.get("nullable", False):
+    if result.get("nullable", False) and "null" not in types:
         result["type"] = [*types, "null"]
 
     if "string" in types and "enum" in result:
         result.pop("enum")
 
     return result
-
-
-# class ServiceTitanOpenAPISchema(OpenAPISchema):
-#     """ServiceTitan OpenAPI schema."""
-
-#     @override
-#     @cached_property
-#     def spec(self) -> dict[str, Any]:  # noqa: C901
-#         """Get the spec."""
-#         spec = super().spec
-#         schemas = spec["components"]["schemas"]
-
-#         # Accounting
-#         if "Accounting.V2.JournalEntryResponse" in schemas:
-#             props = schemas["Accounting.V2.JournalEntryResponse"]["properties"]
-#             # Values are coming in as '2025-10-13T00:00:00', so not valid ISO dates
-#             props["postDate"]["format"] = "date-time"
-#         if "Accounting.V2.JournalEntrySummaryResponse" in schemas:
-#             props = schemas["Accounting.V2.JournalEntrySummaryResponse"]["properties"]
-#             # Values are coming in as '2025-10-13T00:00:00', so not valid ISO dates
-#             props["postDate"]["format"] = "date-time"
-#         if "Accounting.V2.JournalEntryDetailsResponse" in schemas:
-#             props = schemas["Accounting.V2.JournalEntryDetailsResponse"]["properties"]
-#             # Values are coming in as '2025-10-13T00:00:00', so not valid ISO dates
-#             props["postDate"]["format"] = "date-time"
-
-#         # CRM
-#         if "Crm.V2.AddressModel" in schemas:
-#             props = schemas["Crm.V2.AddressModel"]["properties"]
-#             props["street"]["nullable"] = True
-#             props["city"]["nullable"] = True
-#             props["state"]["nullable"] = True
-#             props["zip"]["nullable"] = True
-#             props["country"]["nullable"] = True
-
-#         if "Crm.V2.Customers.CustomerAddress" in schemas:
-#             props = schemas["Crm.V2.Customers.CustomerAddress"]["properties"]
-#             props["country"]["nullable"] = True
-
-#         if "Crm.V2.ExportCustomerResponse" in schemas:
-#             props = schemas["Crm.V2.ExportCustomerResponse"]["properties"]
-#             props["externalData"]["nullable"] = True
-
-#         if "Crm.V2.ExportLocationsResponse" in schemas:
-#             props = schemas["Crm.V2.ExportLocationsResponse"]["properties"]
-#             props["externalData"]["nullable"] = True
-
-#         # Dispatch
-#         if "Dispatch.V2.NonJobAppointmentResponse" in schemas:
-#             props = schemas["Dispatch.V2.NonJobAppointmentResponse"]["properties"]
-#             props["summary"]["nullable"] = True
-
-#         # JPM
-#         if "CustomFieldApiModel" in schemas:
-#             props = schemas["CustomFieldApiModel"]["properties"]
-#             props["value"]["nullable"] = True
-
-#         if "Jpm.V2.ExportJobsResponse" in schemas:
-#             props = schemas["Jpm.V2.ExportJobsResponse"]["properties"]
-#             props["customerPo"]["nullable"] = True
-#             props["externalData"]["nullable"] = True
-
-#         if "Jpm.V2.ExportProjectsResponse" in schemas:
-#             props = schemas["Jpm.V2.ExportProjectsResponse"]["properties"]
-#             props["externalData"]["nullable"] = True
-
-#         if "Jpm.V2.JobTypeResponse" in schemas:
-#             props = schemas["Jpm.V2.JobTypeResponse"]["properties"]
-#             props["externalData"]["nullable"] = True
-
-#         # Telecom
-#         if "Telecom.V2.ExportCallResponse" in schemas:
-#             props = schemas["Telecom.V2.ExportCallResponse"]["properties"]
-#             props["excuseMemo"]["nullable"] = True
-#             props["tag"]["nullable"] = True
-
-#         return spec
-
-#     @override
-#     def fetch_schema(self, key: str) -> dict[str, Any]:
-#         """Fetch the schema for the given key."""
-#         schema = super().fetch_schema(key)
-#         return _normalize_schema(schema)
 
 
 class ServiceTitanSchema(StreamSchema):
