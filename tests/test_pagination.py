06375c48b980373a7c3ebb5ea884e2610540e869
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-10-14 16:28:31 -0600

    fix: Do not default to start capacities sync 14 days ago

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -26,76 +26,22 @@ def test_capacities_paginator(subtests: SubTests) -> None:
     seven_days_from_now = fake_now + timedelta(days=7)
     response = Mock(spec=requests.Response)
 
-    with subtests.test("Start Date within 14 day range"), warnings.catch_warnings():
-        warnings.simplefilter("error")
-        with time_machine.travel(fake_now):
-            paginator = CapacitiesPaginator(start_value=three_days_ago)
+    with time_machine.travel(fake_now):
+        paginator = CapacitiesPaginator(start_value=three_days_ago)
 
-        # Should start at 3 days ago
-        assert paginator.current_value == three_days_ago
-        assert paginator.end_value == seven_days_from_now
-        assert paginator.has_more(response=response)
+    # Should start at 3 days ago
+    assert paginator.current_value == three_days_ago
+    assert paginator.end_value == seven_days_from_now
+    assert paginator.has_more(response=response)
 
+    paginator.advance(response=response)
+    assert paginator.current_value == three_days_ago + timedelta(days=1)
+    assert paginator.has_more(response=response)
+
+    for _ in range(10):  # from 3 days ago to 7 days from now
         paginator.advance(response=response)
-        assert paginator.current_value == three_days_ago + timedelta(days=1)
-        assert paginator.has_more(response=response)
 
-        for _ in range(10):  # from 3 days ago to 7 days from now
-            paginator.advance(response=response)
+    assert not paginator.has_more(response=response)
 
-        assert not paginator.has_more(response=response)
-
-        # Final value should be 1 day after the end value
-        assert paginator.current_value - paginator.end_value == timedelta(days=1)
-
-    with subtests.test("Start Date outside 14 day range"):
-        with (
-            time_machine.travel(fake_now),
-            pytest.warns(
-                UserWarning,
-                match=(
-                    r"Provided start value at '2025-01-05 00:00:00\+00:00' is outside "
-                    "of the 14 day range limit, setting it to "
-                    r"'2025-01-11 00:00:00\+00:00'"
-                ),
-            ),
-        ):
-            paginator = CapacitiesPaginator(start_value=twenty_days_ago)
-
-        # Should respect the range limit of 14 days
-        assert paginator.current_value == fourteen_days_ago
-        assert paginator.end_value == seven_days_from_now
-        assert paginator.has_more(response=response)
-
-        paginator.advance(response=response)
-        assert paginator.current_value == fourteen_days_ago + timedelta(days=1)
-        assert paginator.has_more(response=response)
-
-        for _ in range(21):  # from 14 days ago to 7 days from now
-            paginator.advance(response=response)
-
-        assert not paginator.has_more(response=response)
-
-        # Final value should be 1 day after the end value
-        assert paginator.current_value - paginator.end_value == timedelta(days=1)
-
-    with subtests.test("Start Date is None"):
-        with time_machine.travel(fake_now), warnings.catch_warnings():
-            warnings.simplefilter("error")
-            paginator = CapacitiesPaginator(start_value=None)
-
-        assert paginator.current_value == fourteen_days_ago
-        assert paginator.end_value == seven_days_from_now
-        assert paginator.has_more(response=response)
-
-        paginator.advance(response=response)
-        assert paginator.current_value == fourteen_days_ago + timedelta(days=1)
-        assert paginator.has_more(response=response)
-
-        for _ in range(21):  # from 14 days ago to 7 days from now
-            paginator.advance(response=response)
-
-        assert not paginator.has_more(response=response)
-
-        # Final value should be 1 day after the end value
-        assert paginator.current_value - paginator.end_value == timedelta(days=1)
+    # Final value should be 1 day after the end value
+    assert paginator.current_value - paginator.end_value == timedelta(days=1)
