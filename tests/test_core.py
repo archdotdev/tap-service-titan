66a5101e2358275cba83093cadb2453f38d5dbeb
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-10-21 13:14:00 -0600

    Restore test config

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,14 +11,13 @@ from tap_service_titan.tap import TapServiceTitan
 dotenv.load_dotenv()
 
 SAMPLE_CONFIG = {
-    # "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "start_date": "2025-06-01",
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
     "client_id": environ["TAP_SERVICE_TITAN_CLIENT_ID"],
     "client_secret": environ["TAP_SERVICE_TITAN_CLIENT_SECRET"],
     "st_app_key": environ["TAP_SERVICE_TITAN_ST_APP_KEY"],
     "tenant_id": environ["TAP_SERVICE_TITAN_TENANT_ID"],
-    # "api_url": "https://api-integration.servicetitan.io",
-    # "auth_url": "https://auth-integration.servicetitan.io/connect/token",
+    "api_url": "https://api-integration.servicetitan.io",
+    "auth_url": "https://auth-integration.servicetitan.io/connect/token",
 }
 
 
@@ -30,7 +29,7 @@ TestTapServiceTitan = get_tap_test_class(
         max_records_limit=25,
     ),
     # Enable these to run the tests
-    # include_stream_attribute_tests=False,
-    # include_stream_tests=False,
-    # include_tap_tests=False,
+    include_stream_attribute_tests=False,
+    include_stream_tests=False,
+    include_tap_tests=False,
 )
