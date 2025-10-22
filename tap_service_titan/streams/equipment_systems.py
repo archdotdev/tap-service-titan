05903be49680b4e1ac54b06994dfa20e78f7c6ae
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-10-22 17:34:03 -0600

    feat: Pull equipment systems schemas from the OpenAPI spec (#152)

--- tap_service_titan/streams/equipment_systems.py
+++ tap_service_titan/streams/equipment_systems.py
@@ -2,13 +2,16 @@
 
 from __future__ import annotations
 
+import sys
 from functools import cached_property
 
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from tap_service_titan.client import ServiceTitanStream
+from tap_service_titan.openapi_specs import EQUIPMENT_SYSTEMS, ServiceTitanSchema
 
-from tap_service_titan.client import (
-    ServiceTitanStream,
-)
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
 
 
 class InstalledEquipmentStream(ServiceTitanStream):
@@ -17,47 +20,12 @@ class InstalledEquipmentStream(ServiceTitanStream):
     name = "installed_equipment"
     primary_keys = ("id",)
     replication_key: str = "modifiedOn"
+    schema = ServiceTitanSchema(
+        EQUIPMENT_SYSTEMS,
+        key="EquipmentSystems.V2.InstalledEquipmentResponse",
+    )
 
-    schema = th.PropertiesList(
-        th.Property("id", th.IntegerType),
-        th.Property("equipmentId", th.IntegerType),
-        th.Property("locationId", th.IntegerType),
-        th.Property("customerId", th.IntegerType),
-        th.Property("invoiceItemId", th.IntegerType),
-        th.Property("name", th.StringType),
-        th.Property("installedOn", th.DateTimeType),
-        th.Property("createdOn", th.DateTimeType),
-        th.Property("modifiedOn", th.DateTimeType),
-        th.Property("serialNumber", th.StringType),
-        th.Property("barcodeId", th.StringType),
-        th.Property("memo", th.StringType),
-        th.Property("manufacturer", th.StringType),
-        th.Property("model", th.StringType),
-        th.Property("cost", th.NumberType),
-        th.Property("manufacturerWarrantyStart", th.DateTimeType),
-        th.Property("manufacturerWarrantyEnd", th.DateTimeType),
-        th.Property("serviceProviderWarrantyStart", th.DateTimeType),
-        th.Property("serviceProviderWarrantyEnd", th.DateTimeType),
-        th.Property(
-            "tags",
-            th.ArrayType(
-                th.ObjectType(
-                    th.Property("id", th.IntegerType),
-                    th.Property("ownerId", th.IntegerType),
-                    th.Property("typeId", th.IntegerType),
-                    th.Property("typeName", th.StringType),
-                    th.Property("memo", th.StringType),
-                    th.Property("color", th.StringType),
-                    th.Property("textColor", th.StringType),
-                    th.Property("code", th.StringType),
-                )
-            ),
-        ),
-        th.Property("actualReplacementDate", th.DateType),
-        th.Property("predictedReplacementMonths", th.IntegerType),
-        th.Property("predictedReplacementDate", th.DateType),
-    ).to_dict()
-
+    @override
     @cached_property
     def path(self) -> str:
         """Return the API path for the stream."""
