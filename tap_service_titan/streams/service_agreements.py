0f36742880429fa75882d98dd2c7c2d4a1254149
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-10-22 17:28:52 -0600

    feat: Pull service agreement schemas from the OpenAPI spec (#151)

--- tap_service_titan/streams/service_agreements.py
+++ tap_service_titan/streams/service_agreements.py
@@ -2,13 +2,17 @@
 
 from __future__ import annotations
 
+import sys
 from functools import cached_property
 from typing import TYPE_CHECKING
 
-from singer_sdk import typing as th  # JSON Schema typing helpers
-from typing_extensions import override
-
 from tap_service_titan.client import ServiceTitanExportStream
+from tap_service_titan.openapi_specs import SERVICE_AGREEMENTS, ServiceTitanSchema
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
 
 if TYPE_CHECKING:
     from singer_sdk.helpers.types import Context, Record
@@ -20,65 +24,12 @@ class ServiceAgreementsStream(ServiceTitanExportStream):
     name = "service_agreements"
     primary_keys = ("id",)
     replication_key: str = "modifiedOn"
-    schema = th.PropertiesList(
-        th.Property("id", th.IntegerType),
-        th.Property("name", th.StringType),
-        th.Property("createdOn", th.DateTimeType),
-        th.Property("createdById", th.IntegerType),
-        th.Property("modifiedOn", th.DateTimeType),
-        th.Property("modifiedById", th.IntegerType),
-        th.Property(
-            "templateFields",
-            th.ObjectType(
-                th.Property("originalTemplateId", th.IntegerType),
-                th.Property("revenueRecognitionMode", th.StringType),
-                th.Property("generalLedgerAccountId", th.IntegerType),
-                th.Property("invoiceCancellationMode", th.StringType),
-                th.Property("billingServiceId", th.IntegerType),
-                th.Property("negativeLiabilityServiceId", th.IntegerType),
-                th.Property("positiveIncomeServiceId", th.IntegerType),
-                th.Property("defaultLaborCostPerHour", th.NumberType),
-                th.Property("defaultExtraTravelCostPerHour", th.NumberType),
-                th.Property("defaultLaborMarkupPercentage", th.NumberType),
-                th.Property("defaultMaterialMarkupPercentage", th.NumberType),
-                th.Property("defaultLaborSurcharge", th.NumberType),
-                th.Property("defaultMaterialSurcharge", th.NumberType),
-                th.Property("defaultRateSheetId", th.IntegerType),
-                th.Property("defaultCampaignId", th.IntegerType),
-            ),
-        ),
-        th.Property("customerId", th.IntegerType),
-        th.Property("businessUnitId", th.IntegerType),
-        th.Property("status", th.StringType),
-        th.Property("locationIds", th.ArrayType(th.IntegerType)),
-        th.Property("origin", th.StringType),
-        th.Property("summary", th.StringType),
-        th.Property("importId", th.StringType),
-        th.Property("importedDeferredAmount", th.NumberType),
-        th.Property("importedRecognizedAmount", th.NumberType),
-        th.Property("durationMonths", th.IntegerType),
-        th.Property("autoRenew", th.BooleanType),
-        th.Property("renewalAgreementId", th.IntegerType),
-        th.Property("startDate", th.DateType),
-        th.Property("endDate", th.DateType),
-        th.Property("billingSchedule", th.StringType),
-        th.Property("paymentTermId", th.IntegerType),
-        th.Property("paymentMethodId", th.IntegerType),
-        th.Property("estimatedTravelCost", th.NumberType),
-        th.Property("estimatedLaborCost", th.NumberType),
-        th.Property("estimatedMaterialCost", th.NumberType),
-        th.Property("laborMarkupPercentage", th.NumberType),
-        th.Property("materialMarkupPercentage", th.NumberType),
-        th.Property("laborSurcharge", th.NumberType),
-        th.Property("materialSurcharge", th.NumberType),
-        th.Property("totalAgreementPrice", th.NumberType),
-        th.Property("estimatedGrossMarginAmount", th.NumberType),
-        th.Property("estimatedGrossMarginPercentage", th.NumberType),
-        th.Property("rateSheetId", th.IntegerType),
-        th.Property("accountManagerId", th.IntegerType),
-        th.Property("active", th.BooleanType),
-    ).to_dict()
+    schema = ServiceTitanSchema(
+        SERVICE_AGREEMENTS,
+        key="ServiceAgreements.V2.ExportServiceAgreementResponse",
+    )
 
+    @override
     @cached_property
     def path(self) -> str:
         """Return the API path for the stream."""
